79678d6d0ab59613ee433b610f145b6aabd54c4a
owencodes <43074374+owenbcoding@users.noreply.github.com>
2024-05-31 02:22:43 +0100

    updated task description to proper task

--- classes-poperties-methods.php
+++ classes-poperties-methods.php
@@ -1,10 +1,11 @@
 <!-- 
     Task
-1 - Define a class called Person.
-2 -  Inside the Person class, declare (but do not define) the two public properties, called $first_name and $last_name respectively.
-3 - Define a class constructor which accepts exactly two arguments in the following order, $first_name and $last_name. The argument $first_name should be stored into the $first_name property of the object being instantiated and the $last_name argument should be stored into the $last_name property of the object.
-4 - Define a public method called get_full_name which accepts no arguments and returns the full name of the individual in the following format: "FIRST_NAME LAST_NAME". Please note that if the properties ($first_name and/or $last_name) of said object is modified after it is instantiated, 
-5 -  your method get_full_name should return the updated full name, not the original full name of the individual. -->
+1 - Define a class called President using the syntax taught above. 
+2 - Inside the curly braces of the class definition, add a public property called $name and set it equal to the string "Barack Obama".
+3 - Then, define a public method inside the President class called greet which accepts one argument $name and returns something to the effect of "Hello INSERT_NAME_HERE, my name is Barack Obama, nice to meet you!"
+4 - Now that you have defined your class, create an instance of that class (called an object) and store it in a variable called $us_president.
+5 - Store the name of the $us_president into another variable called $president_name.
+6 -Store the result of calling the greet method of $us_president with the argument "Donald" into the variable $greetings_from_president. -->
 
 <?php
 class President { 
