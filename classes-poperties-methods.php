d7c1b781c126e2e8d96c5902b343851b45afbf79
owencodes <43074374+owenbcoding@users.noreply.github.com>
2024-05-31 02:20:43 +0100

    completed classes, properties and method challenge

--- classes-poperties-methods.php
+++ classes-poperties-methods.php
@@ -6,3 +6,14 @@
 4 - Define a public method called get_full_name which accepts no arguments and returns the full name of the individual in the following format: "FIRST_NAME LAST_NAME". Please note that if the properties ($first_name and/or $last_name) of said object is modified after it is instantiated, 
 5 -  your method get_full_name should return the updated full name, not the original full name of the individual. -->
 
+<?php
+class President { 
+  public $name = "Barack Obama";
+  public function greet($name){ 
+    return "Hello $name, my name is Barack Obama, nice to meet you!";
+  }
+}
+$us_president = new President;
+$president_name = $us_president->name;
+$greetings_from_president = $us_president->greet("Donald");
+?>
